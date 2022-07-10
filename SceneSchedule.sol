595d44f6f39df7373f9ae1515476e5ccc58f4097
yeonsik <happyit84@gmail.com>
2022-07-10 21:41:41 +0900

    use timestamp and unit is hour

--- SceneSchedule.sol
+++ SceneSchedule.sol
@@ -38,14 +38,39 @@ contract FeeCache {
     }
 }
 
+contract ScheduleInfo {
+    uint public startTimestamp; // inclusive
+    uint public endTimestamp; // exclusive
+    address booker;
+    string public data;
+
+    constructor (uint _startTimestamp, uint _endTimestamp, address _booker, string memory _data) {
+        startTimestamp = _startTimestamp;
+        endTimestamp = _endTimestamp;
+        booker = _booker;
+        data = _data;
+    }
+}
+
 contract SceneSchedule is Ownable {
     FeeCache private fee;
+    ScheduleInfo [] schedules;
+    mapping(uint => uint) scheduleMap; // each starting hour => index in schedules
+    uint constant NotReserved = 0; // value for representing not reserved in scheduleMap
 
     constructor() {
         fee = new FeeCache();
     }
 
-    function setFee(uint256 newFeeWeiPerSecond) public {
+    function getTimestampNow() public view returns (uint) {
+        return block.timestamp;
+    }
+
+    function balance() public view onlyOwner returns (uint) {
+        return address(this).balance;
+    }
+    
+    function setFee(uint256 newFeeWeiPerSecond) public onlyOwner {
         fee.setFee(newFeeWeiPerSecond);
     }
 
@@ -65,5 +90,36 @@ contract SceneSchedule is Ownable {
         return fee.getFeePerDay();
     }
 
-    
+    function createSchedule(uint _startTimestamp, uint _endTimestamp, string memory _data) external payable returns (uint256 scheduleIndex) {
+        // check if timestamp is hour base
+        require(_startTimestamp >= block.timestamp, "_startTimestamp should not be past time.");
+        require(_startTimestamp % 3600 == 0, "_startTimestamp should point at starting of each hour.");
+        require(_endTimestamp % 3600 == 0, "_endTimestamp should point at the starting of each hour.");
+        require(_startTimestamp < _endTimestamp, "_startTimestamp should be earlier than _endTimpstamp.");
+
+        uint hourCount = (_endTimestamp - _startTimestamp) / 3600;
+
+        if (msg.value < hourCount * getFeePerHour())
+            revert("ETH amount is not enough to create schedule for given period.");
+        else if (msg.value > hourCount * getFeePerHour())
+            revert("ETH amount is too much to create schedule for given period.");
+
+
+        for (uint t = _startTimestamp; t < _endTimestamp ; t += 3600) {
+            if (NotReserved != scheduleMap[t]) 
+                revert("There's already reserved time.");
+        }
+
+        ScheduleInfo info = new ScheduleInfo(_startTimestamp, _endTimestamp, msg.sender, _data);
+        schedules.push(info);
+        scheduleIndex = schedules.length - 1;
+
+        for (uint t = _startTimestamp; t < _endTimestamp ; t += 3600) {
+            scheduleMap[t] = scheduleIndex;
+        }
+
+        payable(address(this)).transfer(msg.value);
+
+        return scheduleIndex;
+    }
 }
